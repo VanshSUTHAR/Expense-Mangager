76e14d80e5c89fc2d39d231f45209cf7fe7760e1
VanshSUTHAR <sutharvansh022@gmail.com>
2026-06-09 11:31:55 +0530

    update api.js

--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -1,32 +1,9 @@
-// import axios from 'axios';
-
-// const api = axios.create({
-//   baseURL: '/api',
-// });
-
-// api.interceptors.request.use((config) => {
-//   const token = localStorage.getItem('token');
-//   if (token) config.headers.Authorization = `Bearer ${token}`;
-//   return config;
-// });
-
-// api.interceptors.response.use(
-//   (res) => res,
-//   (error) => {
-//     if (error.response?.status === 401) {
-//       localStorage.removeItem('token');
-//       window.location.href = '/login';
-//     }
-//     return Promise.reject(error);
-//   }
-// );
-
-// export default api;
-
 import axios from "axios";
 
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api";
+
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: API_BASE_URL,
   withCredentials: true,
 });
 
